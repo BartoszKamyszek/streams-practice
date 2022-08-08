a66b4f61f08bad663087232938346f8ecbf193b0
Bartosz Kamyszek <bartoszkamyszek1@gmail.com>
2022-08-08 19:58:06 +0200

    super zmiana dodana

--- src/main/java/com/example/demo/example/Example1.java
+++ src/main/java/com/example/demo/example/Example1.java
@@ -1,10 +1,13 @@
 package com.example.demo.example;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Example1 {
 
     public static List<Person> findPeopleOlderThan30(List<Person> people) {
-        return null;
+        return people.stream()
+                .filter(p -> p.getAge() > 30)
+                .collect(Collectors.toList());
     }
 }
